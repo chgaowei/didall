9de1752db5eb1c6b9474c9f0e5605c14dc9b3718
changshan <chgaowei@gmail.com>
2024-10-15 22:02:05 +0800

    fix version error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="didall",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         'ecdsa>=0.19.0', 
